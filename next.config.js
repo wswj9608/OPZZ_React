0da516e91b69aff5ad224cb29e513cc3102f98e0
wswj9608 <wswj9608@naver.com>
2023-02-14 09:40:18 +0900

    Add : @svgr/webpack 추가

--- next.config.js
+++ next.config.js
@@ -11,6 +11,14 @@ const nextConfig = {
       'opgg-static.akamaized.net',
     ],
   },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
