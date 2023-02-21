0013cc3aa87288738e119569103eb6743cb7cac9
wswj9608 <wswj9608@naver.com>
2023-02-21 18:03:49 +0900

    Update : perkIcon 추가

---
+++ assets/images/perkIcons/index.ts
@@ -0,0 +1,53 @@
+import domination from './8100.png'
+import inspiration from './8300.png'
+import precision from './8000.png'
+import resolve from './8400.png'
+import sorcery from './8200.png'
+
+import pressTheAttack from './8005.png'
+import lethalTempoTemp from './8008.png'
+import conqueror from './8010.png'
+import fleetFootwork from './8021.png'
+
+import electrocute from './8112.png'
+import predator from './8124.png'
+import darkHarvest from './8128.png'
+import hailOfBlades from './9923.png'
+
+import summonAery from './8214.png'
+import arcaneComet from './8229.png'
+import phaseRush from './8230.png'
+
+import glacialAugment from './8351.png'
+import unsealedSpellbook from './8360.png'
+import firstStrike from './8369.png'
+
+import graspOfTheUndying from './8437.png'
+import aftershock from './8439.png'
+import guardian from './8465.png'
+import { StaticImageData } from 'next/image'
+
+export const perkIcons: { [key: number]: StaticImageData } = {
+  8100: domination,
+  8300: inspiration,
+  8000: precision,
+  8400: resolve,
+  8200: sorcery,
+  8005: pressTheAttack,
+  8008: lethalTempoTemp,
+  8010: conqueror,
+  8021: fleetFootwork,
+  8112: electrocute,
+  8124: predator,
+  8128: darkHarvest,
+  9923: hailOfBlades,
+  8214: summonAery,
+  8229: arcaneComet,
+  8230: phaseRush,
+  8351: glacialAugment,
+  8360: unsealedSpellbook,
+  8369: firstStrike,
+  8437: graspOfTheUndying,
+  8439: aftershock,
+  8465: guardian,
+}
